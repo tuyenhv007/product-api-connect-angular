de5e9774ede19e1dbf8d1fa3e07f912d90bf27b8
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-07-25 10:58:13 +0700

    test config git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
         <div class="title m-b-md">
             Laravel
         </div>
-
+<h3>Hello</h3>
         <div class="links">
             <a href="https://laravel.com/docs">Docs</a>
             <a href="https://laracasts.com">Laracasts</a>
